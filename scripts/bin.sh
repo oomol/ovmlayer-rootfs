3f3d3f5be8ffcfbb7da620465ee556c89311b3b7
leavesster <11785335+leavesster@users.noreply.github.com>
2025-08-01 19:38:49 +0800

    fix equal check

--- scripts/bin.sh
+++ scripts/bin.sh
@@ -1,16 +1,17 @@
+#!/usr/bin/env bash
 set -x
-echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" > /1.log
-if $TARGETPLATFORM == "linux/amd64"; then
-  echo "Downloading amd64 oocana and ovmlayer" >> /1.log
+echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" > /tmp/1.log
+if [ "$TARGETPLATFORM" == "linux/amd64" ]; then
+  echo "Downloading amd64 oocana and ovmlayer" >> /tmp/1.log
   cp -r ./amd64/oocana /app/
   cp -r ./amd64/ovmlayer /app/
   cp ./amd64/rootfs.tar /root/rootfs.tar
-elif $TARGETPLATFORM == "linux/arm64"; then
-  echo "Downloading arm64 oocana and ovmlayer" >> /1.log
+elif [ "$TARGETPLATFORM" == "linux/arm64" ]; then
+  echo "Downloading arm64 oocana and ovmlayer" >> /tmp/1.log
   cp -r ./arm64/oocana /app/
   cp -r ./arm64/ovmlayer /app/
   cp ./arm64/rootfs.tar /root/rootfs.tar
 else
-  echo "Unsupported platform: $TARGETPLATFORM" >> /1.log
-  exit 1
+  echo "Unsupported platform: $TARGETPLATFORM" >> /tmp/1.log
+  exit 23
 fi
