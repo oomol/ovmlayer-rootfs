e0a3c4816879c9426c70d713e252dc1cdc4d2d45
leavesster <11785335+leavesster@users.noreply.github.com>
2025-12-10 15:41:14 +0800

    feat: support multiple cases (#211)
    * feat: support multiple cases
    
    * Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 set -e
-ovmlayer setup dev --base-rootfs=/root/rootfs.tar --layer-disk=/root/tmp_overlay_desk
+if [ ! -d "/opt/ovmlayer" ]; then
+  echo "[Init] /opt/ovmlayer directory not found, initializing with dev mode."
+  ovmlayer setup dev --base-rootfs=/tmp/rootfs.tar --layer-disk=/root/tmp_overlay_disk
+elif [ -d "/opt/ovmlayer/layer_dir/base_rootfs/" ]; then
+  echo "[Init] Detected existing Layer data, skipping initialization."
+else
+  echo "[Init] Initializing Layer with production configuration."
+  ovmlayer setup production --base-rootfs=/tmp/rootfs.tar
+fi
 exec "$@"
