bc196c50860b99947190277352c7c13133420401
leavesster <11785335+leavesster@users.noreply.github.com>
2025-12-10 15:40:46 +0800

    Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-if [ -d "/opt/ovmlayer/layer_dir/base_rootfs/" ]; then
-  echo "[Init] Detected existing Layer data, skipping initialization."
-elif [ ! -d "/opt/ovmlayer" ]; then
+if [ ! -d "/opt/ovmlayer" ]; then
   echo "[Init] /opt/ovmlayer directory not found, initializing with dev mode."
   ovmlayer setup dev --base-rootfs=/tmp/rootfs.tar --layer-disk=/root/tmp_overlay_disk
+elif [ -d "/opt/ovmlayer/layer_dir/base_rootfs/" ]; then
+  echo "[Init] Detected existing Layer data, skipping initialization."
 else
   echo "[Init] Initializing Layer with production configuration."
   ovmlayer setup production --base-rootfs=/tmp/rootfs.tar
