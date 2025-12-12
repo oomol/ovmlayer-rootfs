97a3201be77270b542fd4b69bbb8318ca6108fde
leavesster <11785335+leavesster@users.noreply.github.com>
2025-12-12 11:17:44 +0800

    feat: read-only file compatible

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 set -e
+
+# 检查是否为只读，如果是，则重新挂载为读写
+if [ -w "/sys/fs/cgroup" ]; then
+    echo "/sys/fs/cgroup is writable"
+else
+    echo "Remounting /sys/fs/cgroup as RW..."
+    mount -o remount,rw /sys/fs/cgroup
+fi
+
+# 某些情况下（特别是 cgroup v2），可能还需要挂载 cgroup2
+mount -t cgroup2 none /sys/fs/cgroup
+
 if [ ! -d "/opt/ovmlayer" ]; then
   echo "[Init] /opt/ovmlayer directory not found, initializing with dev mode."
   ovmlayer setup dev --base-rootfs=/root/rootfs.tar --layer-disk=/root/tmp_overlay_disk
