9e17ec809d281777645235e76dd7ac2047d5c33d
leavesster <11785335+leavesster@users.noreply.github.com>
2026-01-12 17:27:18 +0800

    fix cloud ovmlayer build error

--- scripts/install-executor.sh
+++ scripts/install-executor.sh
@@ -1,11 +1,14 @@
-
 #!/bin/bash
-
+set -euxo pipefail
 cd /root/layer-deps
 
-npm pkg get dependencies | sed -E 's/[[:space:]]*//g; s/[{}]//g; s/:/@/; s/"//g; s/,//g' | while read -r package; do
+npm pkg get dependencies | sed -E 's/[[:space:]]*//g; s/[{}]//g; s/:/@/; s/"//g; s/,//g' | xargs -n 1 | while read -r package; do
     if [[ "$package" == "@oomol/oocana@"* ]]; then
         echo "Skipping $package"
+        continue
+    fi
+    if [[ "$package" == "" ]]; then
+        echo "Skipping empty"
         continue
     fi
     npm install -g "$package"
